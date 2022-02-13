3ce6050a065eac252ec277a139e9e0223e4fc044
BlueSilenceLiu <87072067+BlueSilenceLiu@users.noreply.github.com>
2022-02-13 16:08:17 +0800

    show the remaining time

--- tester.py
+++ tester.py
@@ -8,20 +8,20 @@ total_time = 0
 for i in range(len(dw)):
     wordle = Wordle(dw[i], mxt=100)
     solver = Solver()
+    cb = perf_counter()
     while True:
         type_in = solver.get()
         out = wordle.inp(type_in)
         if type(out) == str:
-            cb = perf_counter()
             solver.inp(out, type_in)
-            ce = perf_counter()
-            total_time += ce - cb
         else:
             attempts[wordle.t - 2] += 1
             if wordle.t - 1 <= 6:
                 win += 1
             break
-    print(f"{i+1}/{len(dw)}")
+    ce = perf_counter()
+    total_time += ce - cb
+    print(f"{i+1}/{len(dw)}  remaining: {(len(dw)-i-1)/(i+1)*total_time}s")
 print(attempts)
 print(f"win rate: {win / len(dw) * 100}%")
 print(f"average score: {sum([(i+1)*attempts[i] for i in range(len(attempts))])/len(dw)}")
