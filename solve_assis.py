c33a2cb1e1618d659a5090243464611bcbca9fba
BlueSilenceLiu <87072067+BlueSilenceLiu@users.noreply.github.com>
2022-02-13 15:34:35 +0800

    show the words' entropy

--- solve_assis.py
+++ solve_assis.py
@@ -1,14 +1,17 @@
 from main import RawSolver as Solver, dw
+from entropy import ged
 
-def main(word_set=dw, outputs="CEN"):
+def main(word_set=dw, outputs="CEN", entropy_file_location="default_words.entropy"):
     solver = Solver(word_set, outputs)
+    entropy_dict = ged(entropy_file_location)
+    entropy_dict_r = {e: w for w, e in entropy_dict.items()}
     while True:
         usr_answer = input("your answer")
         res = input("the game's result")
         print("processing...")
         solver.inp(res, usr_answer)
         print("here are all the available answers:")
-        answers = solver.get()
+        answers = [entropy_dict_r[i]+"\t"+str(i) for i in sorted([entropy_dict[i] for i in solver.get()])]
         if len(answers) <= 40:
             print("\n".join(answers))
         else:
