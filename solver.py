4f8a9a7cd4764e3ebab0f22cec0b4ef19bb5acd0
BlueSilenceLiu <87072067+BlueSilenceLiu@users.noreply.github.com>
2022-02-13 15:48:32 +0800

    make the avg guess time fewer and fewer

--- solver.py
+++ solver.py
@@ -1,17 +1,14 @@
-from main import dw, RawSolver, choice
+from main import dw, RawSolver
+from entropy import ged
 
 class Solver:
-    def __init__(self, word_set=dw, outputs="CEN"):
+    def __init__(self, word_set=dw, outputs="CEN", entropy_file_location="default_words.entropy"):
         self.rsv = RawSolver(word_set, outputs)
         self.full_set = word_set
+        self.ent = ged(entropy_file_location)
 
     def inp(self, res, word):
         self.rsv.inp(res, word)
 
     def get(self):
-        if self.rsv.available == self.full_set:
-            return "adieu"
-        else:
-            return choice(self.rsv.get())
-
-
+        return max(zip(self.ent.values(), self.ent.keys()))[1]
